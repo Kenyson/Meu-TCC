c55953548f738fc79268349090c05c1b32394f38
Kenyson <101434524+Kenyson@users.noreply.github.com>
2023-05-10 16:27:36 -0300

    feature-TCC-001.01
    O código foi alterado de modo a ser mais abrangente e generalista para ser, em seguida, reutilizado em outra parte.

--- pms/src/app/components/pms-grid/pms-grid.component.ts
+++ pms/src/app/components/pms-grid/pms-grid.component.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
 
+interface Paciente {
+  nome: string;
+  medicamento: string;
+  indicacao: string;
+  posologia: string;
+}
+
+interface Coluna {
+  nome: string;
+  propriedade: keyof Paciente;
+}
+
 @Component({
   selector: 'app-pms-grid',
   templateUrl: './pms-grid.component.html',
   styleUrls: ['./pms-grid.component.css']
 })
 export class PmsGridComponent {
+  pacientes: Paciente[] = [
+    { nome: 'João', medicamento: 'Paracetamol', indicacao: 'Febre', posologia: '1 comprimido a cada 6 horas' },
+    { nome: 'Maria', medicamento: 'Dipirona', indicacao: 'Dor de cabeça', posologia: '1 comprimido a cada 8 horas' },
+    { nome: 'Pedro', medicamento: 'Amoxicilina', indicacao: 'Infecção', posologia: '1 comprimido a cada 12 horas' },
+  ];
 
-  pacientes = [
-    { nome: 'João', medicamento: 'Silva', indicacao: '30', posologia: '25' },
-    { nome: 'Maria', medicamento: 'Santos', indicacao: '25', posologia: '25' },
-    { nome: 'Lucas', medicamento: 'Rodrigues', indicacao: '40', posologia: '25' }
+  colunas: Coluna[] = [
+    { nome: 'Nome Paciente', propriedade: 'nome' },
+    { nome: 'Medicamento', propriedade: 'medicamento' },
+    { nome: 'Indicação', propriedade: 'indicacao' },
+    { nome: 'Posologia', propriedade: 'posologia' },
   ];
 
   adicionarPaciente() {
@@ -19,5 +37,5 @@ export class PmsGridComponent {
 
   newPaciente() {
     console.log('Botão clicado!');
-  };
+  }
 }
