edc722a02321ba278e81344ad1c33f015aa3fc98
Kenyson <101434524+Kenyson@users.noreply.github.com>
2023-05-27 16:33:43 -0300

    feature-TCC-001.10
    Criadas as rotas de navegação da aplicação.

--- pms/src/app/app-routing.module.ts
+++ pms/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { PmsMedicoComponent } from './components/pms-medico/pms-medico.component';
+import { PmsPacienteComponent } from './components/pms-paciente/pms-paciente.component';
+import { PmsNewPacienteComponent } from './components/pms-new-paciente/pms-new-paciente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'cadastro',
+    component:CadastroComponent
+  },
+  {
+    path:'medico',
+    component:PmsMedicoComponent
+  },
+  {
+    path:'paciente',
+    component:PmsPacienteComponent
+  },
+  {
+    path:'newPaciente',
+    component:PmsNewPacienteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
