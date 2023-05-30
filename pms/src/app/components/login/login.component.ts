24508ae73083376be794781b6e14e17bda90e6ed
Kenyson <101434524+Kenyson@users.noreply.github.com>
2023-05-29 21:01:19 -0300

    feature-TCC-001.17
    Testes de Login

--- pms/src/app/components/login/login.component.ts
+++ pms/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,24 +10,72 @@ export class LoginComponent {
   selectedOption: string;
   estados: string[];
   selectedEstado: string;
+  mensagemErro: string;
 
-
-  constructor() {
+  constructor(private router: Router) {
     this.selectedOption = 'medico';
     this.estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
     this.selectedEstado = '';
+    this.mensagemErro = '';
   }
 
   changeOption(option: string) {
     this.selectedOption = option;
+    this.mensagemErro = '';
+  }
+
+  testLogin(){
+    if (this.selectedOption === 'medico') {
+
+      const crm = (document.getElementById('crm') as HTMLInputElement).value;
+      const estado = this.selectedEstado;
+
+
+      if (crm === '123456' && estado === 'São Paulo') {
+
+       return '/medico'
+
+      } else {
+
+
+        return '/login'
+      }
+    } else if (this.selectedOption === 'paciente') {
+
+
+      const cpf = (document.getElementById('cpf') as HTMLInputElement).value;
+
+
+      if (cpf === '12345678900') {
+
+        return '/paciente'
+      } else {
+
+
+        return '/login'
+      }
+    }return '/login'
   }
 
   submitForm() {
-
     if (this.selectedOption === 'medico') {
 
+      const crm = (document.getElementById('crm') as HTMLInputElement).value;
+      const estado = this.selectedEstado;
+
+      if (crm === '123456' && estado === 'São Paulo') {
+        this.router.navigate(['/pagina-medico']);
+      } else {
+        this.mensagemErro = 'Credenciais inválidas. Por favor, tente novamente.';
+      }
     } else if (this.selectedOption === 'paciente') {
 
+      const cpf = (document.getElementById('cpf') as HTMLInputElement).value;
+      if (cpf === '12345678900') {
+        this.router.navigate(['/pagina-paciente']);
+      } else {
+        this.mensagemErro = 'Credenciais inválidas. Por favor, tente novamente.';
+      }
     }
   }
 }
