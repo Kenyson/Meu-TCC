d4078ad1f6ce6b28c5f8ba537bc9b270739e7501
Kenyson <kenysonoliveira@gmail.com>
2026-05-31 01:38:29 +0200

    feature-TCC-002.11
    Ajustes

--- pms/src/main.ts
+++ pms/src/main.ts
@@ -1,9 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic([
-  { provide: LocationStrategy, useClass: HashLocationStrategy }
-]).bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
